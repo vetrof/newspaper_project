5a1b5bd46a98ce304b75155379dfe8521d4a32f5
Vitaly Vetrof <vetrof@gmail.com>
2023-06-03 18:33:00 +0600

    article

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from articles.views import ArticleListView
+from articles.views import ArticleListView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView, ArticleCreateView
 
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='article_list')
+    path('', ArticleListView.as_view(), name='article_list'),
+    path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
+    path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
+    path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
+    path('new/', ArticleCreateView.as_view(), name='article_new'),
 ]
