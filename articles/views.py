61456b4281509719ca1f4ce5695df28ca8e9668f
Vitaly Vetrof <vetrof@gmail.com>
2023-06-04 12:46:27 +0600

    mixin

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
@@ -5,29 +6,39 @@ from django.views.generic import ListView, DetailView, UpdateView, DeleteView, C
 from articles.models import Article
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin,ListView):
     model = Article
     template_name = 'article_list.html'
+    login_url = 'login'
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin,DetailView):
     model = Article
     template_name = 'article_detail.html'
+    login_url = 'login'
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin,UpdateView):
     model = Article
     fields = ['title', 'body']
     template_name = 'article_edit.html'
+    login_url = 'login'
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin,DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    login_url = 'login'
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = "article_new.html"
-    fields = ("title", "body", "author")
+    fields = ("title", "body")
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
